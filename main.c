49f2f3e10492ae0f7d061c22138400193e109b71
mmiller <martin.miller@stud.htwk-leipzig.de>
2024-03-08 17:59:29 +0100

    addded minute Timer with Interrupt

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
-#include <avr/interrupt.h >
 #include <avr/io.h >
+#include <avr/interrupt.h >
 #include <stdint.h>
+#include <util/delay.h>
+
+#define TICKS_PER_SECOND 16000000UL
 
 volatile uint16_t ms;
 volatile uint8_t s;
@@ -8,11 +11,15 @@ volatile uint8_t min;
 volatile uint8_t h;
 
 void main () {
-    TCCR0B |= (1 << CS01 );     // ps =8
-    OCR0A = 255 - 1;
-    TCCR0A |= (1 << WGM01 );    // enable CTC
-    TIMSK0 |= (1 << OCIE0A );   // enable compare interrupt 1A
-    sei() ;                     // enable global interrupts
+    DDRC |= (1 << PC0) | (1 << PC1) | (1 << PC2) | (1 << PC3) |(1 << PC4) |(1 << PC5);
+
+    PORTC = 0x00;
+
+    TCCR1B |= (1 << WGM12);                 // CTC-Modus aktivieren
+    OCR1A = TICKS_PER_SECOND / 1000 - 1;    // Vergleichswert für 1ms
+    TIMSK1 |= (1 << OCIE1A);                // Compare-Match-Interrupt aktivieren
+    TCCR1B |= (1 << CS10) | (1 << CS11);    // Prescaler 64
+    sei();                                  // enable global interrupts
 }
 
 ISR (TIMER0_COMPA_vect) {
@@ -27,10 +34,12 @@ ISR (TIMER0_COMPA_vect) {
         if(s >= 60) {
             s=0;
             min++;
+            PORTC++;
 
             //Actions every hour
             if(min >= 60) {
                 min=0;
+                PORTC = 0x00;
                 h++;
 
                 //Actions every day
