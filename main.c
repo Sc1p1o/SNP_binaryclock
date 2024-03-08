1e19029648177a84aac645731e4c24b10308772b
mmiller <martin.miller@stud.htwk-leipzig.de>
2024-03-08 16:08:41 +0100

    edited TIMER0 interrupt

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
-#include <stdio.h>
+#include <avr/interrupt.h >
+#include <avr/io.h >
+#include <stdint.h>
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+volatile uint16_t ms;
+volatile uint8_t s;
+volatile uint8_t min;
+volatile uint8_t h;
+
+void main () {
+    TCCR0B |=(1 << CS01 ) ; // ps =8
+    OCR0A =255-1;
+    TCCR0A |=(1 << WGM01 ) ;// enable CTC
+    TIMSK0 |=(1 << OCIE0A ) ;// enable compare interrupt 1A
+    sei () ;// enable global interrupts
+}
+
+ISR (TIMER0_COMPA_vect) {
+    ms++;
+
+    //actions every second
+    if(ms >= 1000) {
+        ms=0;
+        s++;
+
+        //Actions every minute
+        if(s >= 60) {
+            s=0;
+            min++;
+
+            //Actions every hour
+            if(min >= 60) {
+                min=0;
+                h++;
+
+                //Actions every day
+                if(h >= 24) {
+
+                }
+            }
+        }
+    }
 }
